6542d200da787e9fa8f86e6679ff770e176ffe1d
Ryan Mok <mokryan17@gmail.com>
2019-07-28 23:04:42 -0500

    Limited the total of posts rendered and included an extra piece of data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -14,8 +14,17 @@ class Blog extends Component {
     componentDidMount() {
         axios.get("https://jsonplaceholder.typicode.com/posts")
             .then(response => {
+                const posts = response.data.splice(0, 8);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: "Ryan"
+                    }
+                });
+                // What happens here is the amount of posts have now been limited to only show 8 of the 100 total; the get method is still collecting everything though
+                // Additionally, we're transforming the data just a touch to include a hard coded author
                 this.setState({
-                    posts: response.data
+                    posts: updatedPosts
                 });
                 // The change in state needs to be placed within the promise due to the fact the data is gathered asynchronously.
                 // If placed outside the promise it wont work b/c the data wouldn't have been collected yet!
@@ -29,6 +38,7 @@ class Blog extends Component {
         const posts = this.state.posts.map(post => {
             return <Post
                 title={post.title}
+                author={post.author}
                 key={post.id}
             />
             // Because of the componentDidMount method called before the render method, we can now collect the data from the URL
