dd66e4464f068be342ce5760321a5a98263a5b27
Ryan Mok <mokryan17@gmail.com>
2019-07-28 23:46:53 -0500

    Full post is now rendering proper data, but it constantly performing get requests...

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -4,23 +4,33 @@ import './FullPost.css';
 
 class FullPost extends Component {
 
+    state = {
+        loadedPost: null
+    }
+
     componentDidUpdate() {
         if (this.props.id) {
             axios.get("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
                 .then(response => {
-                    console.log(response)
+                    this.setState({
+                        loadedPost: response.data
+                    })
+                    console.log(response);
                 });
         }
     };
-    // Because we don't want the get method to trigger immediately, we place the if statement before to prevent any unnecesarry processing
+    // Because we don't want the get method to trigger immediately, we place the if statement before to prevent any unnecesarry processing.
 
     render() {
         let post = <p style={{ textAlign: "center", fontWeight: "bold" }}>Please select a Post!</p>;
         if (this.props.id) {
+            post = <p style={{ textAlign: "center", fontWeight: "bold" }}>Pulling up your post!</p>;
+        }
+        if (this.state.loadedPost) {
             post = (
                 <div className="FullPost">
-                    <h1>Title</h1>
-                    <p>Content</p>
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
                         <button className="Delete">Delete</button>
                     </div>
