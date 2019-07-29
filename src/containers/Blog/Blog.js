b7774d2f87ca36f7183a7c7b903d992a11533563
Ryan Mok <mokryan17@gmail.com>
2019-07-28 22:09:16 -0500

    Setting up the get request via axios

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-
+import axios from "axios";
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
+
+    componentDidMount() {
+        axios.get("https://jsonplaceholder.typicode.com/posts")
+    };
+    // We use the componentDidMount method b/c the http request is condiered as a "Side-effect"
+    // The fetching of data is considered a side effect because it's not neccesarrily triggering a rerender of the component; all that it's doing is updating data
+
+    render() {
         return (
             <div>
                 <section className="Posts">
