f8322183043d0ff1bb3ed3c54b806e1fdb066272
Ryan Mok <mokryan17@gmail.com>
2019-07-29 00:04:56 -0500

    Fixed issue where get request was running infinitely

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -10,14 +10,19 @@ class FullPost extends Component {
 
     componentDidUpdate() {
         if (this.props.id) {
-            axios.get("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
-                .then(response => {
-                    this.setState({
-                        loadedPost: response.data
-                    })
-                    console.log(response);
-                });
-        }
+            if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+                axios.get("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
+                    .then(response => {
+                        this.setState({
+                            loadedPost: response.data
+                        })
+                        console.log(response);
+                    });
+            };
+            // There needs to be a secondary if statement nesting the get request to prevent an infinite loop from being triggered.
+            // Because we would be updating the state, the componentDidMount would cause the request to infinitely run.
+            // Additionally, the secondary if statement is checking for the initial state of loadedPost or if the initial post id has changed.
+        };
     };
     // Because we don't want the get method to trigger immediately, we place the if statement before to prevent any unnecesarry processing.
 
@@ -37,6 +42,8 @@ class FullPost extends Component {
                 </div>
             );
         };
+        // There needs to be an additional if statement before the if statement that renders the component because the get request is happening asynchronously.
+        // With the additional check the get request can process, and then the rendering of the component is set to happen after the state change has been established.
         return post;
     }
 }
