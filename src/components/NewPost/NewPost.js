8b6ba7fdb0af28b847b7a9fe6e8fb33ed16edc19
Ryan Mok <mokryan17@gmail.com>
2019-07-29 21:34:36 -0500

    Added some comments about post method, and improved legibility of the newPost JSX

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -20,17 +20,36 @@ class NewPost extends Component {
                 console.log(response)
             });
     };
+    // In this case because the server that's providing the data can't save anything, we are able to at least see that the post is being seen and recorded to the console.
+    // Remember back to the first udemy example in terms of how all the user input can be rendered dynamically!
 
     render() {
         return (
             <div className="NewPost">
                 <h1>Add a Post</h1>
                 <label>Title</label>
-                <input type="text" value={this.state.title} onChange={(event) => this.setState({ title: event.target.value })} />
+                <input
+                    type="text"
+                    value={this.state.title}
+                    onChange={(event) =>
+                        this.setState({
+                            title: event.target.value
+                        })}
+                />
                 <label>Content</label>
-                <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({ content: event.target.value })} />
+                <textarea rows="4"
+                    value={this.state.content}
+                    onChange={(event) =>
+                        this.setState({
+                            content: event.target.value
+                        })} />
                 <label>Author</label>
-                <select value={this.state.author} onChange={(event) => this.setState({ author: event.target.value })}>
+                <select
+                    value={this.state.author}
+                    onChange={(event) =>
+                        this.setState({
+                            author: event.target.value
+                        })}>
                     <option value="Max">Max</option>
                     <option value="Manu">Manu</option>
                 </select>
