876e13f5a737f1fcff8f39c0d331adafed67b191
Ryan Mok <mokryan17@gmail.com>
2019-07-29 21:55:19 -0500

    Added in a message to account for any errors that may occur

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ class Blog extends Component {
 
     state = {
         posts: [],
-        selectedPostID: null
+        selectedPostID: null,
+        error: false
     };
 
     componentDidMount() {
@@ -30,7 +31,14 @@ class Blog extends Component {
                 // The change in state needs to be placed within the promise due to the fact the data is gathered asynchronously.
                 // If placed outside the promise it wont work b/c the data wouldn't have been collected yet!
                 console.log(response);
-            });
+            })
+            .catch(err => {
+                this.setState({
+                    error: true
+                })
+                // Sometime there might be a screw up that prevents the code from working properly...What you can do is set up a CATCH method to at least
+                // look for the error and display a message saying something went wrong!
+            })
     };
     // We use the componentDidMount method b/c the http request is condiered as a "Side-effect"
     // The fetching of data is considered a side effect because it's not neccesarrily triggering a rerender of the component; all that it's doing is updating data
@@ -42,17 +50,23 @@ class Blog extends Component {
     };
 
     render() {
-        const posts = this.state.posts.map(post => {
-            return <Post
-                title={post.title}
-                author={post.author}
-                clicked={() => this.postSelectedHandler(post.id)}
-                key={post.id}
-            />
-            // Because of the componentDidMount method called before the render method, we can now collect the data from the URL
-            // What we then do after changing the state to include all the data from the URL is to save it into a new array to maintain immutable data and then map it out
-            // Finally, the mapped data is then utilized in the Post component by calling the data through the props method and then rendered via the constant posts
-        });
+        let posts = <p style={{ textAlign: "center" }}>Something went wrong (╯°@°)╯︵ ┻━┻</p>
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)}
+                    key={post.id}
+                />
+                // Because of the componentDidMount method called before the render method, we can now collect the data from the URL
+                // What we then do after changing the state to include all the data from the URL is to save it into a new array to maintain immutable data and then map it out
+                // Finally, the mapped data is then utilized in the Post component by calling the data through the props method and then rendered via the constant posts
+            });
+        };
+        // To account for the possibility of an error, we needed to reorganize this part specifically just to display the error in the event there is an issue with some part of the method.
+        // It's always good to account for the possibility of an error because it'll allow the developer (i.e. you) to try and keep an eye on your code
+
         return (
             <div>
                 <section className="Posts">
