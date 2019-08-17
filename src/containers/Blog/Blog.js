842e5417e6d647307f1dfc7c4e245616790a7aa1
Ryan Mok <mokryan17@gmail.com>
2019-08-17 01:24:13 -0500

    Moved the conditinal rendering methods into the Posts component

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,12 +5,6 @@ import './Blog.css';
 
 class Blog extends Component {
 
-    state = {
-        posts: [],
-        selectedPostID: null,
-        error: false
-    };
-
     componentDidMount() {
         axios.get("/posts")
             // Note: Normally the full url would be placed here (https://jsonplaceholder.typicode.com), but in this case we established a global URL in index.js that handles it form us
@@ -49,23 +43,6 @@ class Blog extends Component {
     };
 
     render() {
-        let posts = <p style={{ textAlign: "center" }}>Something went wrong (╯°w°)╯︵ ┻━┻</p>
-        if (!this.state.error) {
-            posts = this.state.posts.map(post => {
-                return <Post
-                    title={post.title}
-                    author={post.author}
-                    clicked={() => this.postSelectedHandler(post.id)}
-                    key={post.id}
-                />
-                // Because of the componentDidMount method called before the render method, we can now collect the data from the URL
-                // What we then do after changing the state to include all the data from the URL is to save it into a new array to maintain immutable data and then map it out
-                // Finally, the mapped data is then utilized in the Post component by calling the data through the props method and then rendered via the constant posts
-            });
-        };
-        // To account for the possibility of an error, we needed to reorganize this part specifically just to display the error in the event there is an issue with some part of the method.
-        // It's always good to account for the possibility of an error because it'll allow the developer (i.e. you) to try and keep an eye on your code
-
         return (
             <div className="Blog">
                 <header>
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,7 +1,29 @@
 import React, { Component } from "react";
 
 class Posts extends Component {
+    state = {
+        posts: [],
+        selectedPostID: null,
+        error: false
+    };
+
     render() {
+        let posts = <p style={{ textAlign: "center" }}>Something went wrong (╯°w°)╯︵ ┻━┻</p>
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)}
+                    key={post.id}
+                />
+                // Because of the componentDidMount method called before the render method, we can now collect the data from the URL
+                // What we then do after changing the state to include all the data from the URL is to save it into a new array to maintain immutable data and then map it out
+                // Finally, the mapped data is then utilized in the Post component by calling the data through the props method and then rendered via the constant posts
+            });
+        };
+        // To account for the possibility of an error, we needed to reorganize this part specifically just to display the error in the event there is an issue with some part of the method.
+        // It's always good to account for the possibility of an error because it'll allow the developer (i.e. you) to try and keep an eye on your code
         return (
             <section className="Posts">
                 {posts}
