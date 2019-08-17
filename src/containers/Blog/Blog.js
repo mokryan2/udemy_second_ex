2e5ade60b00e17e3fc58862ac5285f334ddb428e
Ryan Mok <mokryan17@gmail.com>
2019-08-17 01:20:04 -0500

    Started work on Posts component

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import axios from "axios";
 import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
@@ -78,17 +76,6 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost
-                        id={this.state.selectedPostID}
-                    />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
             </div>
         );
     }
---
+++ src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Posts extends Component {
+    render() {
+        return (
+            <section className="Posts">
+                {posts}
+            </section>
+        )
+    }
+}
+
+export default Posts;
