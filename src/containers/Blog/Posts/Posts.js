40d07a6409ea2946cab023e9b3bdb9c0955d035b
Ryan Mok <mokryan17@gmail.com>
2019-08-17 01:26:41 -0500

    Moved componentDidMount and handlers into posts

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -2,9 +2,44 @@ import React, { Component } from "react";
 
 class Posts extends Component {
     state = {
-        posts: [],
-        selectedPostID: null,
-        error: false
+        posts: []
+    };
+
+    componentDidMount() {
+        axios.get("/posts")
+            // Note: Normally the full url would be placed here (https://jsonplaceholder.typicode.com), but in this case we established a global URL in index.js that handles it form us
+            .then(response => {
+                const posts = response.data.splice(0, 8);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: "Ryan"
+                    }
+                });
+                // What happens here is the amount of posts have now been limited to only show 8 of the 100 total; the get method is still collecting everything though
+                // Additionally, we're transforming the data just a touch to include a hard coded author
+                this.setState({
+                    posts: updatedPosts
+                });
+                // The change in state needs to be placed within the promise due to the fact the data is gathered asynchronously.
+                // If placed outside the promise it wont work b/c the data wouldn't have been collected yet!
+                console.log(response);
+            })
+            .catch(err => {
+                this.setState({
+                    error: true
+                })
+                // Sometime there might be a screw up that prevents the code from working properly...What you can do is set up a CATCH method to at least
+                // look for the error and display a message saying something went wrong!
+            })
+    };
+    // We use the componentDidMount method b/c the http request is condiered as a "Side-effect"
+    // The fetching of data is considered a side effect because it's not neccesarrily triggering a rerender of the component; all that it's doing is updating data
+
+    postSelectedHandler = (id) => {
+        this.setState({
+            selectedPostID: id
+        });
     };
 
     render() {
