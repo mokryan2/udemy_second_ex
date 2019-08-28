43e82d00f7712b8003b99180bb81cf58798732c7
Ryan Mok <mokryan17@gmail.com>
2019-08-27 23:44:25 -0500

    Added a method to allow conditional redirects via the new post component

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect } from "react-router-dom";
 import axios from "axios";
 import './NewPost.css';
 
@@ -6,7 +7,8 @@ class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Max'
+        author: 'Max',
+        submitted: false
     };
 
     addPostHandler = () => {
@@ -19,14 +21,27 @@ class NewPost extends Component {
             // Note: Normally the full url would be placed here (https://jsonplaceholder.typicode.com), but in this case we established a global URL in index.js that handles it form us
             .then(response => {
                 console.log(response)
+                this.setState({
+                    submitted: true
+                })
             });
     };
     // In this case because the server that's providing the data can't save anything, we are able to at least see that the post is being seen and recorded to the console.
     // Remember back to the first udemy example in terms of how all the user input can be rendered dynamically!
 
     render() {
+
+        let redirect = null;
+
+        if (this.state.submitted) {
+            redirect = <Redirect to="/posts" />
+        }
+        // This is a case example of using the Redirect component granted by react-router-dom. We set up a conditional render here to allow the new post component to redirect
+        // the user to the posts page after submitting the new post
+
         return (
             <div className="NewPost">
+                {redirect}
                 <h1>Add a Post</h1>
                 <label>Title</label>
                 <input
