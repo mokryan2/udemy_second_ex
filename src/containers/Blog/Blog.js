5c925b683b2191e33a21032eb2d73bc5446fbe85
Ryan Mok <mokryan17@gmail.com>
2019-08-27 23:36:53 -0500

    Added in a redirect to push / to /posts

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Posts from "./Posts/Posts"
 import NewPost from "./NewPost/NewPost";
-import { Route, NavLink, Switch } from "react-router-dom";
+import { Route, NavLink, Switch, Redirect } from "react-router-dom";
 // While technically you could just use the regular Link from react-router-dom, NavLink allows for easier application of styles to a link by designating an
 // "active" class to said link. Additionally, you could also use your own styling as inline styling
 import './Blog.css';
@@ -36,6 +36,10 @@ class Blog extends Component {
                 <Switch>
                     <Route path="/new-post" exact component={NewPost} />
                     <Route path="/posts" component={Posts} />
+                    <Redirect from="/" to="/posts" />
+                    {/* Redirect is used as another route that doesn't render content; instead it changes the url to reach another route that does render content.
+                This could technically be replaced with "<Route path="/" component={Posts} />", but because React-router-dom already has this method we might as
+                well use it. ADDITIONALLY, if you place the redirect outside the switch statement, the from field can't be specified!!! */}
                 </Switch>
                 {/* We use the Switch component to load only a SINGLE route of the given options that matches what is picked.
                 This prevents an issue we were having with FullPost and NewPost where the 2 components could be rendered at the same time!
