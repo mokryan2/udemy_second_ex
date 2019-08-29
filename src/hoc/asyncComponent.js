77523c669d0de0406a44040f7cbf1683d1835201
Ryan Mok <mokryan17@gmail.com>
2019-08-28 23:31:57 -0500

    Finished setup of asyncComponent

--- src/hoc/asyncComponent.js
+++ src/hoc/asyncComponent.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 
 const asyncComponent = (importComponent) => {
-    return class extends asyncComponent {
+    return class extends Component {
+        state = {
+            component = null
+        };
 
-    }
+        componentDidMount() {
+            importComponent()
+                .then(cmp => {
+                    this.setState({
+                        component: cmp.default
+                    });
+                });
+        };
+
+        render() {
+            const C = this.state.component;
+
+            return C ? <C {...this.props} /> : null
+        };
+    };
 };
 
 export default asyncComponent;
+
+// This component is being used to load everything ASYNCHRONOUSLY(otherwise known as "lazy loading" to get only the parts that are needed WHEN they are needed).
